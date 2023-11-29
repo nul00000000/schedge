5abcfb715acf0761d22fe12f04d6463b0013d2ed
nul00000000 <nulcreatesgames@gmail.com>
2023-11-29 15:17:44 -0500

    uh oh

--- checkemail/main.js
+++ checkemail/main.js
@@ -3,7 +3,7 @@ var id = urlParams.get("id");
 var a = document.querySelector("#resend");
 a.onclick = function () {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:8383/resendemail");
+    xhr.open("POST", "https://schedge.net/auth/resendemail");
     xhr.setRequestHeader("Content-Type", "text/plain");
     xhr.send(id);
 };
